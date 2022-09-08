7ab2cabc0d981b3d1c0573592c437202c505bd01
Isai Castro <isaicstr@gmail.com>
2022-09-08 05:08:32 -0700

    First commit

---
+++ soccerAPI.js
@@ -0,0 +1,19 @@
+// let apiKey = "a71347195c0d1cc14a27dbf7d1343c89";
+
+// let myHeaders = new Headers();
+// myHeaders.append("x-rapidapi-key", apiKey);
+// myHeaders.append("x-rapidapi-host", "v3.football.api-sports.io");
+
+// var requestOptions = {
+//   method: "GET",
+//   headers: myHeaders,
+//   redirect: "follow",
+// };
+
+// fetch("https://v3.football.api-sports.io/leagues", requestOptions)
+//   .then((response) => response.text())
+//   .then((result) => console.log(result))
+//   .catch((error) => console.log("error", error));
+
+//   v3id = 2     V2id = 4314	World	 UEFA Champions League	2022
+//   v3id = 140	    v2id = 4378	 Spain	 La Liga	2022
